0721060cd1252da796d01b3ad3be5959c58a8b3f
Karl Gohlke <gohlke@me.com>
2025-04-21 13:01:13 +0200

    🐛 Fix DEFAULT_FROM_EMAIL conflict & prepare for email tests

--- cbv_goodwill/settings.py
+++ cbv_goodwill/settings.py
@@ -112,13 +112,13 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# default Emailadresse
-DEFAULT_FROM_EMAIL = "Casa Bella Vista <casabelavista@amt-fuer-liebe-und-dankbarkeit.de>"
-# Email config
+# E-Mail-Konfiguration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.ionos.de'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+
+# 📬 Absenderadresse (nicht nochmal überschreiben!)
+DEFAULT_FROM_EMAIL = "Casa Bella Vista <casabelavista@amt-fuer-liebe-und-dankbarkeit.de>"
